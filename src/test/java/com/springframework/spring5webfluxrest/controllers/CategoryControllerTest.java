f353002c506769c5788f957ff1813d7dbe5d62c5
Daniel Ruiz Cayuela <druiz@financialforce.com>
2020-05-03 22:42:27 +0200

    Add Category Controller Test

---
+++ src/test/java/com/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -0,0 +1,50 @@
+package com.springframework.spring5webfluxrest.controllers;
+
+import com.springframework.spring5webfluxrest.domain.Category;
+import com.springframework.spring5webfluxrest.repositories.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+
+class CategoryControllerTest {
+
+    WebTestClient webTestClient;
+    CategoryRepository categoryRepository;
+    CategoryController categoryController;
+
+    @BeforeEach
+    void setUp() {
+        categoryRepository = Mockito.mock(CategoryRepository.class);
+        categoryController = new CategoryController(categoryRepository);
+        webTestClient = WebTestClient.bindToController(categoryController).build();
+    }
+
+    @Test
+    void list() {
+        BDDMockito.given(categoryRepository.findAll())
+                .willReturn(Flux.just(Category.builder().description("Cat1").build(),
+                        Category.builder().description("Cat2").build()));
+
+        webTestClient.get().uri("/api/v1/categories")
+                .exchange()
+                .expectBodyList(Category.class)
+                .hasSize(2);
+    }
+
+    @Test
+    void getById() {
+        BDDMockito.given(categoryRepository.findById(anyString()))
+                .willReturn(Mono.just(Category.builder().description("Cat").build()));
+
+        webTestClient.get().uri("/api/v1/categories/foo")
+                .exchange()
+                .expectBody(Category.class);
+    }
+}
