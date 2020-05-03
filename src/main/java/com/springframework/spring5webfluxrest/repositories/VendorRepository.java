e231c87b0e79e5893a184ce7c966df8e27611f82
Daniel Ruiz Cayuela <druiz@financialforce.com>
2020-05-03 22:27:39 +0200

    Add Mongo DB

---
+++ src/main/java/com/springframework/spring5webfluxrest/repositories/VendorRepository.java
@@ -0,0 +1,7 @@
+package com.springframework.spring5webfluxrest.repositories;
+
+import com.springframework.spring5webfluxrest.domain.Vendor;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface VendorRepository extends ReactiveMongoRepository<Vendor, String> {
+}
