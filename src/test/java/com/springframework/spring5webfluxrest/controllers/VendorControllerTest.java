ba46ec15e7a98ee7361b25eb13c6b553d3a155fe
Daniel Ruiz Cayuela <druiz@financialforce.com>
2020-05-03 22:55:01 +0200

    Add Vendor Controller and Test

---
+++ src/test/java/com/springframework/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -0,0 +1,50 @@
+package com.springframework.spring5webfluxrest.controllers;
+
+import com.springframework.spring5webfluxrest.domain.Vendor;
+import com.springframework.spring5webfluxrest.repositories.VendorRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+
+class VendorControllerTest {
+
+    WebTestClient webTestClient;
+    VendorRepository vendorRepository;
+    VendorController vendorController;
+
+    @BeforeEach
+    void setUp() {
+        vendorRepository = Mockito.mock(VendorRepository.class);
+        vendorController = new VendorController(vendorRepository);
+        webTestClient = WebTestClient.bindToController(vendorController).build();
+    }
+
+    @Test
+    void list() {
+        BDDMockito.given(vendorRepository.findAll())
+                .willReturn(Flux.just(Vendor.builder().firstName("Fred").lastName("Flinstone").build(),
+                        Vendor.builder().firstName("Barney").lastName("Rubble").build()));
+
+        webTestClient.get().uri("/api/v1/vendors")
+                .exchange()
+                .expectBodyList(Vendor.class)
+                .hasSize(2);
+    }
+
+    @Test
+    void getById() {
+        BDDMockito.given(vendorRepository.findById(anyString()))
+                .willReturn(Mono.just(Vendor.builder().firstName("Fred").lastName("Flinstone").build()));
+
+        webTestClient.get().uri("/api/v1/vendors/foo")
+                .exchange()
+                .expectBody(Vendor.class);
+    }
+}
