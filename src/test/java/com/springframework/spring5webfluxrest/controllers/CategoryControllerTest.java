475c6dd113fe12fa526d9963b4ee819087002b64
Daniel Ruiz Cayuela <druiz@financialforce.com>
2020-05-05 19:12:06 +0200

    Add Update Category method

--- src/test/java/com/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
+++ src/test/java/com/springframework/spring5webfluxrest/controllers/CategoryControllerTest.java
@@ -51,7 +51,7 @@ class CategoryControllerTest {
     }
 
     @Test
-    public void testCreateCategory() {
+    void testCreateCategory() {
         BDDMockito.given(categoryRepository.saveAll(any(Publisher.class)))
                 .willReturn(Flux.just(Category.builder().build()));
 
@@ -63,5 +63,20 @@ class CategoryControllerTest {
                 .exchange()
                 .expectStatus()
                 .isCreated();
+    }
+
+    @Test
+    void testUpdate() {
+        BDDMockito.given(categoryRepository.save(any(Category.class)))
+                .willReturn(Mono.just(Category.builder().build()));
+
+        Mono<Category> catToUpdateMono = Mono.just(Category.builder().description("Some Cat").build());
+
+        webTestClient.put()
+                .uri("/api/v1/categories/foo")
+                .body(catToUpdateMono, Category.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
     }
 }
