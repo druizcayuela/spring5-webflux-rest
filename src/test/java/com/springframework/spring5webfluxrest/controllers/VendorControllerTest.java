3f6fca52e48e42788cdc507344eae94da60a0a7e
Daniel Ruiz Cayuela <druiz@financialforce.com>
2020-05-05 19:21:31 +0200

    Add Update/Create Vendor method

--- src/test/java/com/springframework/spring5webfluxrest/controllers/VendorControllerTest.java
+++ src/test/java/com/springframework/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 
 class VendorControllerTest {
@@ -46,5 +48,35 @@ class VendorControllerTest {
         webTestClient.get().uri("/api/v1/vendors/foo")
                 .exchange()
                 .expectBody(Vendor.class);
+    }
+
+    @Test
+    void testCreateVendor() {
+        BDDMockito.given(vendorRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Vendor.builder().build()));
+
+        Mono<Vendor> vendorToSaveMono = Mono.just(Vendor.builder().firstName("Dani").lastName("Ruiz").build());
+
+        webTestClient.post()
+                .uri("/api/v1/vendors")
+                .body(vendorToSaveMono, Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+    @Test
+    void testUpdate() {
+        BDDMockito.given(vendorRepository.save(any(Vendor.class)))
+                .willReturn(Mono.just(Vendor.builder().build()));
+
+        Mono<Vendor> vendorToUpdateMono = Mono.just(Vendor.builder().firstName("Miguel").lastName("Cano").build());
+
+        webTestClient.put()
+                .uri("/api/v1/vendors/foo")
+                .body(vendorToUpdateMono, Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
     }
 }
